2a13fd6440bf3b24022a51def1e7907fb9320fbe
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-11-24 21:32:45 +0900

    saved prefectures

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getPrefectures } from './lib/api'
 
-function App() {
+const App = () => {
+  const [prefectures, setPrefectures] = useState([])
   useEffect(() => {
-    getPrefectures().then((res) => {
-      console.log(res)
-    })
+    getPrefectures()
+      .then((data) => {
+        // console.log(data)
+        setPrefectures(data)
+      })
+      .catch((err, message) => {
+        console.log(err, message)
+        alert('都道府県を取得できませんでした')
+      })
   }, [])
+
   return (
     <div className="App">
       <header>
         <h1>人口推移～</h1>
-        <div className="content"></div>
       </header>
+      <div className="content">
+        {prefectures.length &&
+          prefectures.map((pref) => {
+            return <span key={pref.prefCode}>{pref.prefName}</span>
+          })}
+      </div>
     </div>
   )
 }
