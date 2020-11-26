8c15cd438771089732aa42bc6fd2f82d03112604
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-11-26 16:42:34 +0900

    調整とコメント

--- src/components/PopulationChart.js
+++ src/components/PopulationChart.js
@@ -1,6 +1,21 @@
 import { useEffect, useState } from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  ReferenceLine
+} from 'recharts'
 
+/**
+ * convert into recharts conpatible interface
+ * @param {Array} objectArray - Array of objects
+ * @return {Array} - recharts conpatible type
+ */
 const groupByYear = (objectArray) => {
   return objectArray
     .reduce((acc, obj) => {
@@ -9,14 +24,20 @@ const groupByYear = (objectArray) => {
         if (index === -1) {
           acc.push({ year: item.year })
         }
+        // actual index
         index = acc.findIndex((i) => i && i.year === item.year)
         acc[index][obj.pref.prefName] = item.value
       })
       return acc
     }, [])
-    .sort((a, b) => a.year - b.year)
+    .sort((a, b) => a.year - b.year) // sort by year
 }
 
+/**
+ * generate unique hex color from string
+ * @param {String} str - string
+ * @return {String} str - hex color code. ex) "#15a081"
+ */
 var stringToColour = (str) => {
   var hash = 0
   for (let i = 0; i < str.length; i++) {
@@ -51,10 +72,23 @@ const PopulationChart = ({ rawData }) => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="year" label={{ value: '[年]', position: 'insideBottomRight', offset: -5 }} />
-          <YAxis label={{ value: '人口 [人]', angle: -90, offset: 80, position: 'insideRight' }} />
-          <Tooltip/>
-          <Legend verticalAlign="top" height={70}/>
+          <XAxis
+            type="number"
+            domain={['dataMin', 'dataMax']}
+            dataKey="year"
+            label={{ value: '[年]', position: 'insideBottomRight', offset: -5 }}
+          />
+          <YAxis
+            label={{
+              value: '人口 [万人]',
+              angle: -90,
+              offset: 80,
+              position: 'insideRight'
+            }}
+            tickFormatter={(v) => v / 10000}
+          />
+          <Tooltip labelFormatter={(year) => `${year}年`} formatter={(value, name, props) => [`${value}人`, name]} />
+          <Legend type="number" domain={['dataMin', 'dataMax']} verticalAlign="top" height={70} />
           {rawData.map((item) => {
             return (
               <Line
@@ -62,11 +96,12 @@ const PopulationChart = ({ rawData }) => {
                 type="monotone"
                 dataKey={item.pref.prefName}
                 stroke={stringToColour(item.pref.prefName)}
-								strokeWidth={2}
-								isAnimationActive={false}
+                strokeWidth={2}
+                isAnimationActive={false}
               />
             )
           })}
+          <ReferenceLine x={new Date().getFullYear()} stroke="#DDD" strokeWidth={2} />
         </LineChart>
       </ResponsiveContainer>
     </div>
